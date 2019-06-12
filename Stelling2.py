ee5a466ba2a503e42a975d4df312aad6e8675449
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2019-06-12 14:32:40 +0200

    Create Stelling2.py

---
+++ Stelling2.py
@@ -0,0 +1,40 @@
+# Barchart met 0-lijn, aanduiding crisis
+
+
+test=df[df["Regions"]=="Nederland"]
+
+full = go.Bar(
+    x = test["Periods"],
+    y = test["verschil in % in aantal verkochte huizen vergeleken met vorig jaar"],
+    name=provincie,
+    base = 0,
+    width = [0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.9,0.9,0.9,0.9,0.9,0.9,0.6,0.6,0.6,0.6,0.6],
+    marker=dict(
+        color=['rgb(80, 66, 244)', 'rgb(80, 66, 244)',
+               'rgb(80, 66, 244)', 'rgb(80, 66, 244)',
+               'rgb(80, 66, 244)', 'rgb(80, 66, 244)',
+               'rgb(80, 66, 244)', 'rgb(80, 66, 244)',
+               'rgb(80, 66, 244)', 'rgb(80, 66, 244)',
+               'rgb(80, 66, 244)', 'rgb(80, 66, 244)',
+               'rgb(80, 66, 244)', 'rgb(206, 20, 29)',
+               'rgb(206, 20, 29)', 'rgb(206, 20, 29)',
+               'rgb(206, 20, 29)', 'rgb(206, 20, 29)',
+               'rgb(206, 20, 29)', 'rgb(80, 66, 244)',
+               'rgb(80, 66, 244)', 'rgb(80, 66, 244)',
+               'rgb(80, 66, 244)'])
+               
+)
+
+data = [full]
+
+layout = go.Layout(
+    showlegend = False,
+    title = 'Aanduiding crisis vanaf 2008 d.m.v. verschil in % in verkochte huizen',
+    yaxis = go.layout.YAxis(
+        title='Verandering in %'
+    )
+    
+)
+
+fig = go.Figure(data=data, layout=layout)
+iplot(fig)
